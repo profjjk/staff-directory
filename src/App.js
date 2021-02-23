f9c37b807a416054a398687b760e288923b2d822
Jordan Kelly <profjjk@gmail.com>
2021-02-22 19:41:55 -0800

    add search field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div>
       <Header />
+      <Search />
     </div>
   );
 };
