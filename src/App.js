493104cf590b068326df7e9e27e1da8cbacaa9f6
Jordan Kelly <profjjk@gmail.com>
2021-02-22 20:47:45 -0800

    add useState and useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Employee from './components/Employee/Employee';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 import Sort from './components/Sort/Sort';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    
+  })
+
   return (
     <div>
       <Header />
